977111613a09c9cf9b7959ff8f1284388aefef5b
Mike Scornavacca <52425912+scornz@users.noreply.github.com>
2024-07-25 12:28:50 -0400

    chore: remove build folders

--- frontend/packages/ui/build/index.d.ts
+++
@@ -1,8 +0,0 @@
-import * as react_jsx_runtime from 'react/jsx-runtime';
-import { TextProps } from 'react-native';
-
-declare const Paragraph: ({ children, style, ...props }: TextProps) => react_jsx_runtime.JSX.Element;
-
-declare const Strong: ({ children, style, ...props }: TextProps) => react_jsx_runtime.JSX.Element;
-
-export { Paragraph, Strong };
